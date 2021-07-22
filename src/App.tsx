f94e4567a71b82003a460978e01e498b830d5027
AirMous3 <airmouselol@gmail.com>
2021-07-22 17:49:43 +0300

    ------ Добавил таски которые буду прокидывать в пропсы

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,25 @@ import './App.css';
 import Todolist from "./components/Todolist";
 
 function App() {
+
+    let tasks = [
+        { id:1 , title: "Redux" , isDone: false },
+        { id:2 , title: "React" , isDone: false },
+        { id:3 , title: "HTML" , isDone: true},
+        { id:4 , title: "Js" , isDone: true},
+        { id:5 , title: "CSS" , isDone: false},
+    ]
+
+    let tasks2 = [
+        { id:1 , title: "Bread" , isDone: true },
+        { id:2 , title: "Milk" , isDone: false },
+        { id:3 , title: "Water" , isDone: false},
+        { id:4 , title: "Chicken" , isDone: true},
+        { id:5 , title: "Chips" , isDone: true},
+    ]
+
     return (
         <div className="App">
-
             <Todolist title={"what to buy"}/>
             <Todolist title={"what to learn"}/>
         </div>
